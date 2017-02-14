4980769834083d5fd27a600f38813b475d1e487c
lee-vincent <vincent.lee@10kbulbs.com>
2017-02-14 12:03:01 -0500

    Add proect files

---
+++ structs.h
@@ -0,0 +1,11 @@
+/* You can modify the structure to store whatever you'd like in your BST */
+
+struct data {
+	int number;
+	char *name;
+};
+
+struct baseballPlayer {
+    int homerunTotal;
+    char *playerName;
+};
